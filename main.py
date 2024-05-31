645bee67f2618d9c4f78af0372311d34fc3a630a
Austin Sehorn <Austinsehorn@gmail.com>
2024-05-31 12:20:12 -0700

    linting

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import os
+"""
+This is a Discord bot that fetches real-time weather information for Edgerton, Kansas,
+and reports it to a Discord server.
+"""
+
 import requests
 from discord.ext import commands
 from discord import Intents
 from dotenv import load_dotenv
+import os
 
 # Load environment variables from .env file
 load_dotenv()
@@ -13,24 +18,32 @@ OPENWEATHERMAP_API_KEY = os.getenv('OPENWEATHERMAP_API_KEY')
 
 # Enable the required intents
 intents = Intents.default()
+intents.message_content = True
 
 # Create the bot instance with the specified intents
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Event handler for when the bot is ready
-'''TODO DOCSTRING'''
 @bot.event
 async def on_ready():
+    """Event handler for when the bot is ready."""
     print(f'Logged in as {bot.user.name}')
 
-# Command handler for !weather
-'''TODO DOCSTRING'''
+@bot.command()
+async def bothi(ctx):
+    """Command handler for !bothi."""
+    await ctx.send('Hello Guys!')
+
 @bot.command()
 async def weather(ctx):
+    """Command handler for !weather."""
     city = 'Edgerton'
     state = 'KS'
     country = 'US'
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid={OPENWEATHERMAP_API_KEY}&units=imperial'
+    url = (
+        f'http://api.openweathermap.org/data/2.5/weather?q={city},{state},'
+        f'{country}&appid={OPENWEATHERMAP_API_KEY}&units=imperial'
+    )
+    
     try:
         response = requests.get(url, timeout=10)  # Adding a timeout of 10 seconds
         response.raise_for_status()  # Raise an exception for HTTP errors
@@ -39,13 +52,16 @@ async def weather(ctx):
         temperature = data['main']['temp']
         humidity = data['main']['humidity']
         wind_speed = data['wind']['speed']
+        
         weather_report = (
             f"🌤️ **Weather in {city}, {state}:**\n"
             f"🌡️ **Temperature:** {temperature}°F\n"
             f"💧 **Humidity:** {humidity}%\n"
             f"🌬️ **Wind Speed:** {wind_speed} mph\n"
             f"📝 **Description:** {weather_description}\n\n"
-            f"‼️ [Tune in to Ryan Hall for urgent weather reports and seek shelter in Adam's basement if Wind Speed is dangerous!](https://www.youtube.com/@RyanHallYall) ‼️"
+            f"‼️ [Check out this YouTube channel!]"
+            f"‼️ [Tune in to Ryan Hall for urgent weather reports and seek shelter in Adam's basement if Wind Speed is dangerous!]
+            (https://www.youtube.com/@RyanHallYall) ‼️"
         )
         await ctx.send(weather_report)
     except requests.exceptions.RequestException as e:
