44aa4360e8c6db7937c567e0a26ac1258cedf1bd
Austin Sehorn <Austinsehorn@gmail.com>
2024-05-31 11:52:40 -0700

    Fixed import order for linting

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
+import os
 from discord.ext import commands
 from discord import Intents
 from dotenv import load_dotenv
-import os
 
 # Load environment variables from .env file
 load_dotenv()
